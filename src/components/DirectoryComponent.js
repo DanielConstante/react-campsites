2f988c0e59b0db083d6bed0d1d2f1302875f9096
Daniel Constante <daniel_constante86@hotmail.com>
2021-07-07 11:56:42 -0500

    Fetch from Server

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDirectoryItem({ campsite }) {
     return (
         <Card>
             <Link to={`/directory/${campsite.id}`}>
-                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImg width="100%" src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardImgOverlay>
                     <CardTitle>{campsite.name}</CardTitle>
                 </CardImgOverlay>
@@ -51,12 +52,12 @@ function Directory(props) {
         <div className="container">
             <div className="row">
                 <div className="col">
-                 <Breadcrumb>
-                 <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                 <BreadcrumbItem active>Directory</BreadcrumbItem>
-                 </Breadcrumb>
-                 <h2>Directory</h2>
-                 <hr />
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
                 </div>
             </div>
             <div className="row">
